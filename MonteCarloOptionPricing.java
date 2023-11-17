cbef737264e2c2949d58c16dbfc34a6cb88f39b5
liwenchu <907630605@qq.com>
2023-11-17 12:09:37 +0800

    modify some comments

--- MonteCarloOptionPricing.java
+++ MonteCarloOptionPricing.java
@@ -2,26 +2,35 @@ package monteCarloOptionPricing;
 
 import java.util.Random;
 
-// 蒙特卡罗模拟可以涉及更多的随机性和复杂的运算
-// 金融领域中的期权定价
-// 期权的价值取决于未来资产价格的不确定性，这可以通过蒙特卡罗模拟来建模
-// 简化的期权定价的蒙特卡罗模拟的示例，用于估算欧式看涨期权的价格
+/*
+ * Monte Carlo simulations can involve more randomness and complex operations
+   Option pricing in the financial sector
+   The value of an option depends on the uncertainty of the future asset price, which can be modeled by Monte Carlo simulations
+   An example of a Monte Carlo simulation of simplified option pricing to estimate the price of a European-style call option
+ */
+/*
+ * Option pricing simulator
+	This program is a Monte Carlo simulator 
+	for option pricing and is used to estimate the price of a European-style call option. 
+	Based on the given initial stock price, exercise price, volatility, risk-free rate, expiration time, etc., 
+	the Monte Carlo simulation method is used to simulate and output the estimated call option price.
+ */
 public class MonteCarloOptionPricing {
 
 	public static void main(String[] args) {
-		double initialStockPrice = Double.parseDouble(args[0]); // 初始股票价格，范围为[1000, 10000]
-		double strikePrice = Double.parseDouble(args[1]); // 行权价格，范围为[1000, 10000]
-		// 保证输入1大于输入2
+		double initialStockPrice = Double.parseDouble(args[0]); // Initial stock price in the range of [1000, 10000]
+		double strikePrice = Double.parseDouble(args[1]); // Strike price in the range of [1000, 10000]
+		// Make sure that input 1 is greater than input 2
 		if (initialStockPrice <= strikePrice) {
 			double tmp = initialStockPrice;
 			initialStockPrice = strikePrice;
 			strikePrice = tmp;
 		}
-		double volatility = Double.parseDouble(args[2]); // 波动率，范围为(0, 1.0)
-		double riskFreeRate = Double.parseDouble(args[3]); // 无风险利率，范围为(0, 1.0)
-		int timeToMaturityInDays = Integer.parseInt(args[4]); // 到期时间（天），范围为[1,30]
+		double volatility = Double.parseDouble(args[2]); // Volatility in the range of (0, 1.0)
+		double riskFreeRate = Double.parseDouble(args[3]); // Risk-free rate, on the scale of (0, 1.0)
+		int timeToMaturityInDays = Integer.parseInt(args[4]); // Expiration time (days) in the range of [1,30]
 
-		int numberOfSimulations = 25; // 模拟次数
+		int numberOfSimulations = 25; // Number of simulations
 
 		double probability = 0.35;
 
@@ -35,7 +44,7 @@ public class MonteCarloOptionPricing {
 	private static double calculateOptionPrice(double initialStockPrice, double strikePrice, double volatility,
 			double riskFreeRate, int timeToMaturityInDays, int numberOfSimulations, double probability) {
 
-		double randomValue = Math.random(); // 生成一个0到1之间的随机数
+		double randomValue = Math.random(); // Generate a random number between 0 and 1
 		Random random;
 		if (randomValue < probability) {
 			random = new Random(123);
@@ -46,24 +55,24 @@ public class MonteCarloOptionPricing {
 		double sumPayoffs = 0.0;
 
 		for (int i = 0; i < numberOfSimulations; i++) {
-			// 模拟股价路径，使用几何布朗运动
+			// Simulate the stock price path using geometric Brownian motion
 			double stockPrice = simulateStockPrice(initialStockPrice, volatility, riskFreeRate, timeToMaturityInDays,
 					random);
 
-			// 计算期权支付
+			// Calculate option payouts
 			double payoff = Math.max(stockPrice - strikePrice, 0.0);
 
 			sumPayoffs += payoff;
 		}
 
-		// 计算期权价格的均值
+		// Calculate the average of the option price
 		return (sumPayoffs / numberOfSimulations) * Math.exp(-riskFreeRate * timeToMaturityInDays / 365.0);
 	}
 
 	private static double simulateStockPrice(double initialStockPrice, double volatility, double riskFreeRate,
 			int timeToMaturityInDays, Random random) {
 
-		double dt = 1.0 / 365.0; // 时间步长，假设每天模拟一次
+		double dt = 1.0 / 365.0; // Time step, assuming a daily simulation
 		double drift = (riskFreeRate - 0.5 * volatility * volatility) * dt;
 		double diffusion = volatility * Math.sqrt(dt);
 
